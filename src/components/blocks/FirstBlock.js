1812104a6986d28a8a2a97ff89cc0986d1e944c9
boketto0 <bekmina.julia@gmail.com>
2023-03-10 02:25:23 +0300

    text

--- src/components/blocks/FirstBlock.js
+++ src/components/blocks/FirstBlock.js
@@ -3,7 +3,9 @@ import './FirstBlock.css'
 function FirstBlock() {
     return(
         <div>
-            <div className='first-block'/>
+            <div className='first-block'>
+                <div className='block-title__text'>Дизайн-система</div>
+            </div>
             <div className='block-circle1'/>
             <div className='block-circle2'/>
         </div>
--- src/components/blocks/SecondBlock.js
+++ src/components/blocks/SecondBlock.js
@@ -8,6 +8,8 @@ import Card5 from '../cards/Card5';
 function SecondBlock() {
     return(
         <div className='second-block'>
+            <div className='second-block__title'>Компоненты</div>
+            <div className='second-block__text1'>База компонентов для создания интерфейсов любой сложности</div>
                 <Card1/>
                 <Card2/>
                 <Card3/>
